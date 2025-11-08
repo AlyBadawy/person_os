c2ed39ac7a884e5112a988daf4e4f3ab51b73e76
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-11-08 13:00:24 -0500

    feat: Add lconversion modules with corresponding specs

---
+++ app/types/eventable_frequency.rb
@@ -0,0 +1,21 @@
+module EventableFrequency
+  NONE = "none"
+  ONCE = "once"
+  DAILY = "daily"
+  WEEKLY = "weekly"
+  BI_WEEKLY = "bi_weekly"
+  CUSTOM_DATES = "custom_dates"
+
+  class << self
+    def none; NONE end
+    def once; ONCE end
+    def daily; DAILY end
+    def weekly; WEEKLY end
+    def bi_weekly; BI_WEEKLY end
+    def custom_dates; CUSTOM_DATES end
+
+    def all
+      [NONE, ONCE, DAILY, WEEKLY, BI_WEEKLY, CUSTOM_DATES]
+    end
+  end
+end
---
+++ spec/types/eventable_frequency_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "EventableFrequency" do
+  describe '.all' do
+    it 'returns all event frequencies' do
+      expect(EventableFrequency.all).to contain_exactly(
+        EventableFrequency.none,
+        EventableFrequency.once,
+        EventableFrequency.daily,
+        EventableFrequency.weekly,
+        EventableFrequency.bi_weekly,
+        EventableFrequency.custom_dates,
+      )
+    end
+  end
+end
