23820c2bd1b5249884daed9ade4e2c0bce9fe9d1
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-11-15 20:55:31 -0500

    feat: Add React dashboard component

--- spec/requests/api/status_spec.rb
+++ spec/requests/api/status_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Statuses", type: :request do
-  describe "GET /show" do
+  describe "GET /" do
     it "returns http success" do
-      get "/api/status/show"
+      sign_in FactoryBot.create(:user)
+      get "/api/status"
       expect(response).to have_http_status(:success)
     end
   end
