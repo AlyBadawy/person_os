f78713982fa4e125036efb5316ae2edbd59ce633
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-11-15 21:23:38 -0500

    feat: Add React dashboard component

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,9 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
+# pin "@hotwired/stimulus", to: "stimulus.min.js"
+# pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+# pin_all_from "app/javascript/controllers", under: "controllers"
+
+# # React bundle compiled by esbuild
+pin "react-bundle", to: "react-bundle.js"
